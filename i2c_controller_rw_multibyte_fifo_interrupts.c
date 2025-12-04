424ede7292561298a71b8c2bc2cd49bf0b682cfc
CHO CHUN HUNG <alan8656123@gmail.com>
2025-12-04 16:40:23 +0800

    open anime

--- i2c_controller_rw_multibyte_fifo_interrupts.c
+++ i2c_controller_rw_multibyte_fifo_interrupts.c
@@ -554,9 +554,119 @@ int main(void)
     // all light////////////////////////////////////////////////
     delay_cycles(4000000);
     //send(55);
-    memset(Lcd_Data,0xFF,40);
-    LCD_IC_DisplayWrite();
 
+    for(uint8_t LcdOpenMode = 0U; LcdOpenMode<=22U;LcdOpenMode++)
+    {
+        if (LcdOpenMode == 0U)
+        {
+            memset(Lcd_Data,0x00,40);
+        }
+        else if (LcdOpenMode == 1U)
+        {
+            LCD_A14 = LCD_A15 = LCD_X4 = LCD_X5 = LCD_X10 = LCD_X11 = 1U;
+        }
+        else if (LcdOpenMode == 2U)
+        {
+            LCD_A13 = LCD_A16 = LCD_A36 = LCD_X51 =1U;
+        }
+        else if (LcdOpenMode == 3U)
+        {
+            LCD_X48 = LCD_12B = LCD_12C = LCD_COL = LCD_13A = LCD_13D = LCD_13E = LCD_13F = LCD_13G = LCD_X3 = LCD_X9 =1U;
+        }
+        else if (LcdOpenMode == 4U)
+        {
+            LCD_A12 = LCD_A17 = 1U;
+        }
+        else if (LcdOpenMode == 5U)
+        {
+            LCD_4B = LCD_4C = LCD_X13 = LCD_X2 = LCD_X8 = LCD_X6 = LCD_X12 = LCD_X14_A = 1U;
+        }
+        else if (LcdOpenMode == 6U)
+        {
+            LCD_A11 = LCD_A18 = LCD_X52 = LCD_X50 = LCD_11A = 1U;
+            LCD_11B = LCD_11C = LCD_12A = LCD_12D = LCD_12E = 1U;
+            LCD_12F = LCD_12G = LCD_13B = LCD_13C = LCD_14A = 1U;
+            LCD_14D = LCD_14E = LCD_14F = LCD_14G = LCD_9A = LCD_9B = LCD_9C = 1U;
+        }
+        else if (LcdOpenMode == 7U)
+        {
+            LCD_A10 = LCD_A19 = LCD_X1 = LCD_X7 = LCD_4A = LCD_4D = LCD_4G = LCD_5E = 1U;
+        }
+        else if (LcdOpenMode == 8U)
+        {
+            LCD_X53 = LCD_X49 = LCD_A9 = LCD_A20 = LCD_A37 = 1U;
+            LCD_A35 = LCD_11D = LCD_11E = LCD_11G = LCD_14B = LCD_14C = LCD_9G = 1U;
+        }
+        else if (LcdOpenMode == 9U)
+        {
+            LCD_X18 = LCD_X14_C = LCD_X14_B = LCD_4E = LCD_4F = LCD_5D = LCD_5F = LCD_5G = 1U;
+        }
+        else if (LcdOpenMode == 10U)
+        {
+            LCD_A8 =LCD_A21 = LCD_9F = LCD_10F = 1U;
+        }
+        else if (LcdOpenMode == 11U)
+        {
+            LCD_X54 = LCD_X47 = LCD_A7 = LCD_A22 = LCD_X30 = 1U;
+            LCD_X31 = LCD_3B = LCD_3C = LCD_3D = LCD_5A = LCD_5C = LCD_DP = LCD_X15 = 1U;
+        }
+        else if (LcdOpenMode == 12U)
+        {
+            LCD_8A = LCD_8B = LCD_10A = LCD_10E = 1U;
+        }
+        else if (LcdOpenMode == 13U)
+        {
+            LCD_A6 = LCD_A23 = LCD_X29 = LCD_3A = LCD_3E = 1U;
+            LCD_3G = LCD_5B = LCD_6D = LCD_6E = LCD_X19 = LCD_X16 = 1U;
+        }
+        else if (LcdOpenMode == 14U)
+        {
+            LCD_X55 = LCD_X46 = LCD_A5 = LCD_A24 = LCD_A38 = LCD_A34 = 1U;
+        }
+        else if (LcdOpenMode == 15U)
+        {
+            LCD_10B = LCD_10G = 1U;
+        }
+        else if (LcdOpenMode == 16U)
+        {
+            LCD_A4 = LCD_A25 = LCD_A29 = LCD_X36 = LCD_2B = 1U;
+            LCD_2C = LCD_2D = LCD_2E = LCD_2G = LCD_3F = 1U;
+            LCD_6C = LCD_6F = LCD_6G = LCD_X22 = LCD_X20 = LCD_X17 = 1U;
+        }
+        else if (LcdOpenMode == 17U)
+        {
+            LCD_A3 = LCD_A26 = LCD_A30 = LCD_1B = LCD_1C = 1U;
+            LCD_1D = LCD_1E = LCD_1G = LCD_2A = LCD_2F = 1U;
+            LCD_6A = LCD_6B = LCD_X32 = LCD_X23 = LCD_X21 = 1U;
+            LCD_9D = LCD_9E = LCD_X25_A = LCD_X56 = LCD_X45 = 1U;
+        }
+        else if (LcdOpenMode == 18U)
+        {
+            LCD_A2 = LCD_A27 = LCD_A31 = LCD_8C = LCD_8G = LCD_10C = LCD_10D = LCD_X42 = 1U;
+        }
+        else if (LcdOpenMode == 19U)
+        {
+            LCD_A1 = LCD_A28 = LCD_A32 = LCD_X57 = LCD_X44 = 1U;
+            LCD_A39 = LCD_A33 = LCD_X38 = LCD_X37 = LCD_X35 = 1U;
+            LCD_X33 = LCD_X28 = LCD_X24 = LCD_1A = LCD_1F = 1U;
+            LCD_7C = LCD_7D = LCD_7E = LCD_7G = LCD_7H = 1U;
+        }
+        else if (LcdOpenMode == 20U)
+        {
+            LCD_8E = LCD_8D = LCD_X41 = LCD_X39 = LCD_X34 = 1U;
+            LCD_X27 = LCD_X25_B = LCD_7A = LCD_7B = LCD_7F = LCD_7H = 1U;
+        }
+        else if (LcdOpenMode == 21U)
+        {
+            LCD_X26 = LCD_X43 = 1U;
+        }
+        else if (LcdOpenMode == 22U) // lcd all on - 1000ms
+        {
+            LCD_X40 = 1U;
+        }
+        delay(50);
+        LCD_IC_DisplayWrite();
+    }
 
      
 //////////////display///////////////////
@@ -566,6 +676,7 @@ int main(void)
     DL_GPIO_setPins(GPIO_CLR_PORT, GPIO_CLR_PIN_CLR_PIN);
     uint8_t display_digit;
 
+    uint8_t SPEED=0;
     uint8_t speed_digit;
     uint8_t speed_ten;
     uint8_t speed_hun;
@@ -597,46 +708,19 @@ int main(void)
 
     //ODO etc off
 
-    hour_ten = NumberToWordTable[1];
-    LCD_11A = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->A);
-    LCD_11B = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->B);
-    LCD_11C = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->C);
-    LCD_11D = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->D);
-    LCD_11E = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->E);
-    //LCD_8F = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->F);
-    LCD_11G = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->G);
-
-    hour_digit = NumberToWordTable[2];
-    LCD_12A = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->A);
-    LCD_12B = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->B);
-    LCD_12C = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->C);
-    LCD_12D = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->D);
-    LCD_12E = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->E);
-    LCD_12F = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->F);
-    LCD_12G = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->G);
-
-    min_ten = NumberToWordTable[0];
-    LCD_13A = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->A);
-    LCD_13B = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->B);
-    LCD_13C = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->C);
-    LCD_13D = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->D);
-    LCD_13E = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->E);
-    LCD_13F = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->F);
-    LCD_13G = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->G);
-
-    min_digit = NumberToWordTable[0];
-    LCD_14A = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->A);
-    LCD_14B = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->B);
-    LCD_14C = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->C);
-    LCD_14D = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->D);
-    LCD_14E = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->E);
-    LCD_14F = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->F);
-    LCD_14G = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->G);
-
-
     while (1){
+        SPEED = count%200;
+        speed_hun = SPEED/100 %10;
+        speed_ten = SPEED/10 %10;
+        speed_digit = SPEED %10;
 
-        speed_hun = NumberToWordTable[(count/100)%2 == 0?16:1];
+
+
+        speed_hun = NumberToWordTable[(speed_hun==0)?16:1];
+        speed_ten = NumberToWordTable[(speed_ten==0&&speed_hun==0)?16:speed_ten];
+        speed_digit = NumberToWordTable[speed_digit];
+
+
         LCD_8A = (((DISPLAY_DIGITAL_TYPE *)&speed_hun)->A);
         LCD_8B = (((DISPLAY_DIGITAL_TYPE *)&speed_hun)->B);
         LCD_8C = (((DISPLAY_DIGITAL_TYPE *)&speed_hun)->C);
@@ -646,7 +730,6 @@ int main(void)
         LCD_8G = (((DISPLAY_DIGITAL_TYPE *)&speed_hun)->G);
 
 
-        speed_ten = NumberToWordTable[((count/10)%10 ==0 && count%200/100==0 )?16:(count/10)%10];
         LCD_9A = (((DISPLAY_DIGITAL_TYPE *)&speed_ten)->A);
         LCD_9B = (((DISPLAY_DIGITAL_TYPE *)&speed_ten)->B);
         LCD_9C = (((DISPLAY_DIGITAL_TYPE *)&speed_ten)->C);
@@ -656,7 +739,6 @@ int main(void)
         LCD_9G = (((DISPLAY_DIGITAL_TYPE *)&speed_ten)->G);
 
 
-        speed_digit = NumberToWordTable[(count)%10];
         LCD_10A = (((DISPLAY_DIGITAL_TYPE *)&speed_digit)->A);
         LCD_10B = (((DISPLAY_DIGITAL_TYPE *)&speed_digit)->B);
         LCD_10C = (((DISPLAY_DIGITAL_TYPE *)&speed_digit)->C);
@@ -807,6 +889,47 @@ int main(void)
                 LCD_1F = LCD_2F = LCD_3F = LCD_4F = LCD_5F = (((DISPLAY_DIGITAL_TYPE *)&display_digit)->F);
                 LCD_1G = LCD_2G = LCD_3G = LCD_4G = LCD_5G = (((DISPLAY_DIGITAL_TYPE *)&display_digit)->G);
             }
+
+            uint8_t hourten_temp=(count/20)%3;
+            uint8_t hour_digit_temp=(count/20)%10;
+            if (hourten_temp ==0)hourten_temp=16;
+            if(hourten_temp ==2)hour_digit_temp%=4;
+
+                hour_ten = NumberToWordTable[hourten_temp];
+                LCD_11A = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->A);
+                LCD_11B = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->B);
+                LCD_11C = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->C);
+                LCD_11D = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->D);
+                LCD_11E = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->E);
+                //LCD_8F = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->F);
+                LCD_11G = (((DISPLAY_DIGITAL_TYPE *)&hour_ten)->G);
+
+                hour_digit = NumberToWordTable[hour_digit_temp];
+                LCD_12A = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->A);
+                LCD_12B = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->B);
+                LCD_12C = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->C);
+                LCD_12D = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->D);
+                LCD_12E = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->E);
+                LCD_12F = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->F);
+                LCD_12G = (((DISPLAY_DIGITAL_TYPE *)&hour_digit)->G);
+
+                min_ten = NumberToWordTable[(count/20)%6];
+                LCD_13A = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->A);
+                LCD_13B = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->B);
+                LCD_13C = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->C);
+                LCD_13D = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->D);
+                LCD_13E = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->E);
+                LCD_13F = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->F);
+                LCD_13G = (((DISPLAY_DIGITAL_TYPE *)&min_ten)->G);
+
+                min_digit = NumberToWordTable[(count/20)%6];
+                LCD_14A = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->A);
+                LCD_14B = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->B);
+                LCD_14C = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->C);
+                LCD_14D = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->D);
+                LCD_14E = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->E);
+                LCD_14F = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->F);
+                LCD_14G = (((DISPLAY_DIGITAL_TYPE *)&min_digit)->G);
         }
 
         if(count%10==0){//5
